ccd2b5588825f58d71ea8b2f5b1249df7bc0d9ce
17o3 <17o3gutk29vinqb@gmail.com>
2022-07-03 12:17:06 +0800

    test vitest

--- test/demo.test.js
+++ test/demo.test.js
@@ -1,11 +1,23 @@
+import { flushPromises, mount } from '@vue/test-utils'
+import Hello from '../src/components/Hello.vue'
 
+test('mount component', async () => {
+  expect(Hello).toBeTruthy()
 
-test('foo snapshot test', () => {
-  const bar = {
-    foo: {
-      x: 0,
-      y: 2,
+  const wrapper = mount(Hello, {
+    props: {
+      count: 4,
     },
-  }
-  expect(bar).toMatchSnapshot()
+  })
+
+  expect(wrapper.text()).toContain('4 x 2 = 8')
+  expect(wrapper.html()).toMatchSnapshot()
+
+  await wrapper.get('button').trigger('click')
+
+  expect(wrapper.text()).toContain('4 x 3 = 12')
+
+  await wrapper.get('button').trigger('click')
+
+  expect(wrapper.text()).toContain('4 x 4 = 16')
 })
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,14 @@
 import { configDefaults,defineConfig } from 'vitest/config'
 import AutoImport from 'unplugin-auto-import/vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   test: {
     exclude: [...configDefaults.exclude, 'packages/template/*'],
+    environment: 'jsdom',
   },
   plugins: [
+    vue(),
     AutoImport({
       imports: ['vitest'],
       dts: true,
