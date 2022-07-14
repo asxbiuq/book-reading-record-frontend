b22bb02807821f32eae53b548a4d451b1305b206
17o3 <17o3gutk29vinqb@gmail.com>
2022-07-14 14:31:54 +0800

    formatTime

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -1,13 +1,17 @@
-import moment from 'moment/min/moment-with-locales'
 import { forEach, forIn, isArray } from 'lodash-es'
+import dayjs from 'dayjs'
+import isLeapYear from 'dayjs/plugin/isLeapYear' // 导入插件
+import 'dayjs/locale/zh-cn' // 导入本地化语言
+import relativeTime from 'dayjs/plugin/relativeTime'
 
-
+dayjs.extend(relativeTime)
+dayjs.extend(isLeapYear)
+dayjs.locale('zh-cn')
 
 export const formatTime = (items:Array<any>) => {
-  moment.locale('zh-cn')
   forEach(items, (item) => {
     if (item.time){
-      item.time = moment(item.time).format('llll')
+      item.time = dayjs().to(item.time)
     }
   })
 }
