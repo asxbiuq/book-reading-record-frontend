dea53fe7e029bb480ca48f6be0e0da8b0cee84a5
17o3 <17o3gutk29vinqb@gmail.com>
2022-07-26 22:04:08 +0800

    cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,14 @@
-
-
 module.exports = {
   plugins: [
     require('tailwindcss/nesting'),
     require('tailwindcss'),
     require('postcss-preset-env')({
       stage: 3,
-      features: { 'nesting-rules': false }
+      features: { 'nesting-rules': false },
     }),
     require('autoprefixer'),
-    require('cssnano'),
-  ]
+    require('cssnano')({
+      preset: 'default',
+    }),
+  ],
 }
